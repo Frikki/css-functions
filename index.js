4311b46aed26c254921e122bd46b99486284a030
frikki <frederik@ishus.is>
2015-06-22 21:36:09 +0200

    Named exports from imports.

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  * @author Frederik Krautwald
  */
 
-export {supportedDeclaration} from './src/supportedDeclaration.js';
-export * from './src/supportedProperty.js';
-export * from './src/isPropertySupported.js';
-export * from './src/supportedValue.js';
+import supportedDeclaration from './src/supportedDeclaration.js';
+import supportedProperty from './src/supportedProperty.js';
+import isPropertySupported from './src/isPropertySupported.js';
+import supportedValue from './src/supportedValue.js';
+
+export {
+    supportedDeclaration,
+    supportedProperty,
+    isPropertySupported,
+    supportedValue
+    };
